24ed74915dd4bc3d894b3739739db6a28365ba3a
FriedRubyjane <wangjinhyun@gmail.com>
2024-02-15 17:33:04 +0300

    Add Auth && Change Name

--- app/styles/header.ts
+++ app/styles/header.ts
@@ -7,12 +7,9 @@ export const styles = StyleSheet.create({
 	},
 	touchable: {
 		flexDirection: 'row',
-		alignItems: 'flex-end',
 	},
 	profileName: {
 		fontWeight: 'bold',
-		paddingTop: 8,
-		paddingBottom: 8,
 		fontSize: 24,
 		lineHeight: 24,
 		color: 'rgb(31 41 55)',
